6233709881bc8ac25433d288524640c824cdc18c
Alexander Schwartz <alexander.schwartz@gmx.net>
2015-10-09 10:20:31 +0200

    make sure no warnings appear in documentation

--- tutorial/build.sh
+++ tutorial/build.sh
@@ -3,4 +3,6 @@ set -e
 mkdir -p $HOME/pages/tutorial
 BASEDIR=$(dirname $0)
 cd $BASEDIR
-asciidoctor -r asciidoctor-diagram tutorial.adoc -d book -D $HOME/pages
+asciidoctor -r asciidoctor-diagram tutorial.adoc -d book -D $HOME/pages | tee asciidoctor.log
+# if there are warnings the build should be marked as unsuccessful
+grep WARNING asciidoctor.log && exit 1
