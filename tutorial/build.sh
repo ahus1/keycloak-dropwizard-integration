ed0c54b1bd1989e48818ca5b5ad29a6f90e9e062
Alexander Schwartz <alexander.schwartz@gmx.net>
2015-10-09 10:25:00 +0200

    don't use exit code of last command as exit code for script

--- tutorial/build.sh
+++ tutorial/build.sh
@@ -6,3 +6,4 @@ cd $BASEDIR
 asciidoctor -r asciidoctor-diagram tutorial.adoc -d book -D $HOME/pages | tee asciidoctor.log
 # if there are warnings the build should be marked as unsuccessful
 grep WARNING asciidoctor.log && exit 1
+exit 0
