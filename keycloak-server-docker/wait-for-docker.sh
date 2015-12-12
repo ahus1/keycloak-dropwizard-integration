10391b74368c46975a66af928e7a420dd668ce61
Alexander Schwartz <alexander.schwartz@gmx.net>
2015-12-12 01:08:50 +0100

    Adding docker to run keycloak inside travis

---
+++ keycloak-server-docker/wait-for-docker.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# when no arguments was given
+if [ $# -eq 0 ]
+then
+    echo "Usage: $0 <host> [port]"
+    exit 1
+fi
+
+HOST=$1
+# port defaults to 8080
+PORT=${2:-8080}
+RETRIES=20
+
+echo -n "Waiting for keycloak to start on ${HOST}:${PORT}"
+# loop until we connect successfully or failed 10 times
+until curl "http://${HOST}:${PORT}/auth" >/dev/null 2>/dev/null
+do
+    RETRIES=$(($RETRIES - 1))
+    if [ $RETRIES -eq 0 ]
+    then
+        echo "Failed to connect"
+        exit 1
+    fi
+    # wait a bit
+    echo -n "."
+    sleep 1
+done
+echo
--- keycloak-server.bat
+++ keycloak-server.bat
@@ -1,1 +1,1 @@
-call keycloak-server/bin/standalone.bat -Dkeycloak.migration.action=import -Dkeycloak.migration.provider=singleFile -Dkeycloak.migration.realmName=test -Dkeycloak.migration.file=keycloak-realm.json -Dkeycloak.migration.strategy=OVERWRITE_EXISTING
+call keycloak-server/bin/standalone.bat -Dkeycloak.migration.action=import -Dkeycloak.migration.provider=singleFile -Dkeycloak.migration.realmName=test -Dkeycloak.migration.file=keycloak-server-docker/keycloak-realm.json -Dkeycloak.migration.strategy=OVERWRITE_EXISTING
