9699afc34b5c471d499ce617ad3ae6906a54beda
Alexander Schwartz <alexander.schwartz@gmx.net>
2016-01-29 18:50:30 +0100

    update to keycloak 1.8

--- keycloak-export.bat
+++ keycloak-export.bat
@@ -1,1 +1,1 @@
-call keycloak-server/bin/standalone.bat -Dkeycloak.migration.action=export -Dkeycloak.migration.provider=singleFile -Dkeycloak.migration.realmName=test -Dkeycloak.migration.file=keycloak-realm.json
+call keycloak-server/bin/standalone.bat -Dkeycloak.migration.action=export -Dkeycloak.migration.provider=singleFile -Dkeycloak.migration.realmName=test -Dkeycloak.migration.file=keycloak-server-docker/keycloak-realm.json
