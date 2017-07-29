ce189d0f69926293754b6f68d07334a5d8168541
Alexander Schwartz <alexander.schwartz@gmx.net>
2017-07-29 23:09:55 +0200

    rename folder to match module name. Update readme and travis-ci

--- tutorial/update-gh-pages.sh
+++ tutorial/update-gh-pages.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 # Taken from and modified http://sleepycoders.blogspot.de/2013/03/sharing-travis-ci-generated-files.html
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   echo -e "Starting to update gh-pages\n"
