a60fcbea569ead558044fb72a800da17e3ea0e7e
Alexander Schwartz <alexander.schwartz@gmx.net>
2017-08-04 12:21:38 +0200

    Updating docs to highlight what need to be done for a custom User implementation #21

--- keycloak-dropwizard-jaxrs-example/src/main/java/de/ahus1/lottery/adapter/dropwizard/LotteryApplication.java
+++ keycloak-dropwizard-jaxrs-example/src/main/java/de/ahus1/lottery/adapter/dropwizard/LotteryApplication.java
@@ -40,6 +40,8 @@ public class LotteryApplication extends Application<LotteryConfiguration> {
             protected KeycloakConfiguration getKeycloakConfiguration(LotteryConfiguration configuration) {
                 return configuration.getKeycloakConfiguration();
             }
+            /* OPTIONAL: override getUserClass(), createAuthorizer() and createAuthenticator() if you want to use
+            * a class other than de.ahus1.keycloak.dropwizard.User to be injected by @User*/
         });
         // end::keycloak[]
 
