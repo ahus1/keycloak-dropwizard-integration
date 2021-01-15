7bd242e993b640debed095beac30375c60dc9c1f
Alexander Schwartz <alexander.schwartz@gmx.net>
2021-01-15 18:09:28 +0100

    updating docs

--- tutorial/build.sh
+++ tutorial/build.sh
@@ -5,7 +5,7 @@ mkdir -p $HOME/pages/tutorial
 BASEDIR=$(dirname $0)
 cd $BASEDIR
 bundle install
-asciidoctor -r asciidoctor-diagram tutorial.adoc -d book -D $HOME/pages | tee asciidoctor.log
+asciidoctor -r asciidoctor-diagram tutorial.adoc -d book -D output | tee asciidoctor.log
 # if there are warnings the build should be marked as unsuccessful
 grep WARNING asciidoctor.log && exit 1
 exit 0
--- tutorial/update-gh-pages.sh
+++ tutorial/update-gh-pages.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -e
+set -x
 echo -e "Starting to update gh-pages\n"
 
 #using token to clone gh-pages branch
@@ -8,13 +9,22 @@ git clone --quiet --branch=gh-pages https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@gith
 git config --global user.email "alexander.schwartz@gmx.net"
 git config --global user.name "GitHub Action"
 
-#go into diractory and copy data we're interested in to that directory
+#go into directory and copy data we're interested in to that directory
+set -e
 cd gh-pages
-cp -Rf $HOME/pages/* .
+cp index.html ../tutorial/output
+rm -rf ./*
+cp -Rf ../tutorial/output/* .
 
 #add, commit and push files
 git add -f .
-git diff --quiet || git commit -m "GitHub build $GITHUB_RUN_ID pushed to gh-pages"
+
+if [ ! -z "$(git status --porcelain)" ]; then
+  git commit -m "GitHub build $GITHUB_RUN_ID pushed to gh-pages"
+else
+  echo no changes
+fi
+
 git push -fq origin gh-pages
 
 echo -e "Done publishing to gh-pages.\n"
